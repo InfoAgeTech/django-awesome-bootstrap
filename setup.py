ff23fe95f5fd6e7dac592379a33355e4488eb7fb
Troy Grosfield <troy.grosfield@gmail.com>
2013-11-25 07:57:49 -0700

    setup file updates

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 import os
+
 from setuptools import setup
+
+
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -18,15 +32,5 @@ setup(
     download_url='https://pypi.python.org/pypi/django-awesome-bootstrap',
     author='Troy Grosfield',
     maintainer='Troy Grosfield',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+    classifiers=classifiers,
 )
