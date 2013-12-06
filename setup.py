9ecb3bf9531019e9a140d2b946e62f0482c83cd2
Troy Grosfield <troy.grosfield@gmail.com>
2013-12-06 06:37:04 -0700

    Back to 3.0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-awesome-bootstrap',
-    version='3.0.3',
+    version='3.0.2',
     packages=['awesome_bootstrap'],
     include_package_data=True,
     license='MIT License',
